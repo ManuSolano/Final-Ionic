2fba0e60bc3c9f2a7c4e8a9406c07562a986a49d
Manuel Solano <0224363@up.edu.mx>
2022-05-30 18:42:01 -0500

    changes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -30,7 +30,7 @@
       </ion-row>
       <ion-row class="ion-justify-content-center">
         <ion-col size="4">
-          <ion-button expand="block" @click="agregarClaves()" href="tabs/tab1">Iniciar Sesión</ion-button>
+          <ion-button expand="block" @click="agregarClaves()" tab="tab1" href="tabs/tab1">Iniciar Sesión</ion-button>
           <alert-controller></alert-controller>
         </ion-col>
       </ion-row>
